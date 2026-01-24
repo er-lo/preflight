9d6351258d03d0a2efcbea05352bc2943cc5dbf2
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-01-24 16:59:53 -0600

    create lambda backend

---
+++ lambda/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+async function handler(event) {
+    console.log(`ENVIRONMENT: ${process.env.NODE_ENVIRONMENT}`);
+    console.log(`EVENT: ${event}`);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello World!'),
+    }
+
+    return response;
+}
+
+exports.handler = handler();
