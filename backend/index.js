0bd05f6ba1e1c2299aac96c70731afe287012527
Erick <contactme@ericklopez.dev>
2026-01-25 16:03:05 -0600

    updated server start, creates db on start

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const dbInitialization = require('./database/initialization/initialization');
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+async function startServer() {
+  // create db tables upon server start
+  const result = await dbInitialization.initializePostgresDatabase();
+  if (!result) {
+    // kill server before it starts if database doesn't connect or create.
+    console.log('[SERVER START] There was an issue creating the database. Cancelling server start.');
+    process.exit(1);
+  }
+  app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
-});
+  });
+}
+
+startServer();
