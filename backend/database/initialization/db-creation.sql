19cc334659c934e1889bc9e0f47fcfd9d0ba7c5b
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-01-29 22:17:48 -0600

    add new fields for db

--- backend/database/initialization/db-creation.sql
+++ backend/database/initialization/db-creation.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS analysis_jobs (
     job_id SERIAL PRIMARY KEY,
     status VARCHAR(20) NOT NULL,
+    schema TEXT NOT NULL,
+    request_payload TEXT NOT NULL,
+    internal_requirements TEXT NOT NULL,
     started_at TIMESTAMP,
     completed_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
