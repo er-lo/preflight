9f3cb3df329869e04846e469c0e946bd11a08d73
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-02-15 22:19:16 -0600

    add validation for create process

---
+++ backend/utils/validation.js
@@ -0,0 +1,17 @@
+function validateAnalysisCreation(schema, payload, requirements) {
+    if(!schema || typeof schema != 'string'){
+        return false;
+    }
+
+    if(!payload || typeof payload != 'string'){
+        return false;
+    }
+
+    if(!requirements || typeof requirements != 'string'){
+        return false;
+    }
+
+    return true;
+}
+
+exports.validateAnalysisCreation = validateAnalysisCreation;
