b5615bd0283c4471c0bd7ac196c41b3788aa4ae5
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-02-16 22:17:04 -0600

    create two new db functions

--- backend/database/analysis.database.js
+++ backend/database/analysis.database.js
@@ -1,6 +1,66 @@
 const dbClient = require('./database.client');
 const { JOB_STATUS, LOG_PREFIXES } = require('../constants/constants')
 
+async function retrieveAnalysisStatus(id) {
+  const pool = await dbClient.getPostgresPool();
+  const client = await pool.connect();
+
+  try{
+    const query = `
+      SELECT *
+      FROM analysis_jobs
+      WHERE job_id = $1
+    `;
+
+    const values = [id];
+
+    const result = await client.query(query, values);
+
+    // TODO: update log
+    if (result.rows.length) {
+      console.log(`${LOG_PREFIXES.CREATE_ANALYSIS_DB} Analysis job was inserted in the DB with id: ${result.rows[0].job_id}`);
+    }
+
+    return result.rows[0];
+
+  } catch (error) {
+    console.log(`${LOG_PREFIXES.CREATE_ANALYSIS_DB} Error: ${error.message}`)
+    return null;
+  } finally {
+    await client.release();
+  }
+}
+
+async function retrieveAnalysisResult(id) {
+  const pool = await dbClient.getPostgresPool();
+  const client = await pool.connect();
+
+  try{
+    const query = `
+      SELECT *
+      FROM analysis_results
+      WHERE job_id = $1
+    `;
+
+    const values = [id];
+
+    const result = await client.query(query, values);
+
+    // TODO: update log
+    if (result.rows.length) {
+      console.log(`${LOG_PREFIXES.CREATE_ANALYSIS_DB} Analysis job was inserted in the DB with id: ${result.rows[0].job_id}`);
+    }
+
+    return result.rows[0];
+
+  } catch (error) {
+    console.log(`${LOG_PREFIXES.CREATE_ANALYSIS_DB} Error: ${error.message}`)
+    return null;
+  } finally {
+    await client.release();
+  }
+}
+
 async function createAnalysisRecord(schema, payload, requirements) {
   const pool = await dbClient.getPostgresPool();
   const client = await pool.connect();
@@ -30,4 +90,6 @@ async function createAnalysisRecord(schema, payload, requirements) {
   }
 }
 
+exports.retrieveAnalysisStatus = retrieveAnalysisStatus;
+exports.retrieveAnalysisResult = retrieveAnalysisResult;
 exports.createAnalysisRecord = createAnalysisRecord;
