c891ee4e06c0f9178598fa0b93cb7dd3eb773cc4
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-02-16 22:17:47 -0600

    update creation process, create retrieval process

--- backend/service/analysis.service.js
+++ backend/service/analysis.service.js
@@ -1,5 +1,44 @@
 const analysisDB = require('../database/analysis.database');
+const awsUtil = require('../utils/aws');
 const validation = require('../utils/validation');
+const { JOB_STATUS } = require('../constants/constants');
+
+async function processAnalysisRetrieval(data) {
+    const { jobId } = data;
+    
+    // since there is only one value no separate validation process is needed
+    if (!jobId) return { success: false, messsage: 'Invalid request. Ensure jobId is included in the request.'};
+
+    const jobStatus = await analysisDB.retrieveAnalysisStatus(jobId);
+    if(!jobStatus) return { success: false, message: 'There was an issue processing your request. Please try again later'};
+
+    switch(jobStatus.status){
+        case(JOB_STATUS.PENDING): 
+            return { success: true, completed: false, message: 'Analysis has not been started yet.'};
+        case(JOB_STATUS.IN_PROGRESS):
+            return { success: true, completed: false, message: 'Analysis is still in progress.'};
+        case(JOB_STATUS.FAILED):
+            return {success: true, completed: false, message: 'There was an issue with the analysis process. Please submit again.' }
+        default:
+            break;
+    }
+
+    const jobResult = await analysisDB.retrieveAnalysisResult(jobId);
+    if(!jobResult) return { success: false, message: 'There was an issue processing your request. Please try again later'};
+
+    return {
+        success: true,
+        completed: true,
+        message: 'Request was successful.',
+        data: {
+            jobId: jobResult.job_id,
+            riskLevel: jobResult.risk_level,
+            summary: jobResult.summary,
+            issues: jobResult.issues_json,
+            recommendations: jobResult.recommendations_json,
+        }
+    };
+}
 
 async function processAnalysisCreation(data) {
     const { schema, payload, requirements } = data;
@@ -10,11 +49,15 @@ async function processAnalysisCreation(data) {
 
     // creates the db record for the analysis
     const jobId = await analysisDB.createAnalysisRecord(schema, payload, requirements);
-    if(!jobId) return { success: false, message: 'Error: There was an issue processing your request. Please try again later.'};
+    if(!jobId) return { success: false, message: 'There was an issue processing your request. Please try again later.'};
 
-    // TODO: send the job ID, along with the payload to lambda
+    // invokes the lambda function asynchronously
+    const lambdaResult = awsUtil.lambdaInvoker(schema, payload, requirements);
+    if(!lambdaResult) return { success: false, message: 'There was an issue processing your request. Please try again later.'}
 
+    // if no errors return try and the job ID
     return { success: true, message: `Your request was successful. Here is your job ID: ${jobId.job_id}. Use this to request your result.`, jobId: jobId.job_id};
 }
 
+exports.processAnalysisRetrieval = processAnalysisRetrieval;
 exports.processAnalysisCreation = processAnalysisCreation;
