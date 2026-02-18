97a714534dbfb708a39efcf36d82d27c23d70cec
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-02-17 21:34:44 -0600

    update logs

--- backend/database/initialization/initialization.js
+++ backend/database/initialization/initialization.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
-const dbHelper = require('../database.client')
-const { LOG_PREFIXES } = require('../../constants/constants')
+const dbHelper = require('../database.client');
+const { LOG_PREFIXES } = require('../../constants/constants');
+const { log } = require('../../utils/log');
+
+const { DB_INIT } = LOG_PREFIXES;
 
 async function initializePostgresDatabase() {
   const pool = await dbHelper.getPostgresPool();
   const client = await pool.connect();
   try {
     const migration = fs.readFileSync('./database/initialization/db-creation.sql', 'utf8');
-    console.log(`${LOG_PREFIXES.DB_INIT} STARTING DB SET UP`)
+    log(DB_INIT, 'STARTING DB SET UP');
     await client.query(migration);
-    console.log(`${LOG_PREFIXES.DB_INIT} DB SET UP COMPLETE`)
+    log(DB_INIT, 'DB SET UP COMPLETE');
     return true;
   } catch (error) {
-    console.log(`${LOG_PREFIXES.DB_INIT} Error: ${error.message}`);
+    log(DB_INIT, `Error: ${error.message}`);
     return false;
   } finally {
     await client.release();
