134874a6c1346db89c6c3f5176c1c341406d3a53
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-02-17 21:37:05 -0600

    create log formatting utility

---
+++ backend/utils/log.js
@@ -0,0 +1,18 @@
+const { LOG_PREFIXES } = require('../constants/constants');
+
+const maxTagLength = Math.max(
+    ...Object.values(LOG_PREFIXES).map(tag => tag.length)
+);
+
+const TAG_WIDTH = maxTagLength + 2;
+
+function formatLog(tag, message) {
+  const formattedTag = `${tag}`.padEnd(TAG_WIDTH, ' ');
+  return `${formattedTag} ${message}`;
+}
+
+function log(tag, message) {
+  console.log(formatLog(tag, message));
+}
+
+exports.log = log;
