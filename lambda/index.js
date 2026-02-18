cc7e0e307a8938f375ea134ddefe50cef2f48499
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-02-17 22:09:35 -0600

    format changes

--- lambda/index.js
+++ lambda/index.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
 async function handler(event) {
-    console.log(`ENVIRONMENT: ${process.env.NODE_ENVIRONMENT}`);
-    console.log(`EVENT: ${event}`);
+  console.log(`ENVIRONMENT: ${process.env.NODE_ENVIRONMENT}`);
+  console.log(`EVENT: ${event}`);
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello World!'),
-    }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello World!'),
+  }
 
-    return response;
+  return response;
 }
 
 exports.handler = handler();
