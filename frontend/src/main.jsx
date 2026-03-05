c8309588ad4454a00616215722a0e33ae9e5c1e8
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2026-03-05 09:26:35 -0600

    set up theme provider and router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.jsx'
 
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#384f6e',
+    },
+    secondary: {
+      main: '#575757',
+    },
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
